95ad1239acdb392718e26e2a0a767d5b8d6c5b67
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2025-01-16 09:31:11 +0100

    Add TMPro and Singleton utility

---
+++ Assets/Scripts/Ronda/Networking/Player.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace KKL.Ronda.Networking
+{
+    public class Player : NetworkBehaviour
+    {
+        private readonly NetworkVariable<FixedString32Bytes> _playerName = new();
+        private readonly NetworkVariable<uint> _score = new();
+    }
+}
